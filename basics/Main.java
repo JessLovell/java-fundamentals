c63668e6b6fd136b069f5fab726b818f6d0e55f3
Jessica Lovell <jessicaamberlovell@gmail.com>
2018-11-27 09:17:01 -0800

    complete pluralize method

--- basics/Main.java
+++ basics/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        String greeting = "Welcome to my Java program.";
+        String greeting = "Welcome";
         System.out.println(greeting);
+        System.out.println(pluralize(greeting, 1));
+    }
+
+    public static String pluralize(String word, int count){
+        if (count == 0 || count > 1){
+            return word + "s";
+        } else {
+            return word;
+        }
     }
 }
