00ae3808a9fbe3fc862e502e21dac86c5c600a3e
Jessica Lovell <jessicaamberlovell@gmail.com>
2018-11-27 10:46:40 -0800

    complete flipping coins feature

--- basics/Main.java
+++ basics/Main.java
@@ -1,8 +1,15 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         String greeting = "Welcome";
         System.out.println(greeting);
-        System.out.println(pluralize(greeting, 1));
+
+        System.out.println("pluralize: "+ pluralize(greeting, 0));
+        System.out.println("pluralize: "+ pluralize(greeting, 1));
+        System.out.println("pluralize: "+ pluralize(greeting, 2));
+
+        flipNHeads(4);
     }
 
     public static String pluralize(String word, int count){
@@ -11,5 +18,35 @@ public class Main {
         } else {
             return word;
         }
+    }
+
+    public static void flipNHeads(int nHeads){
+        int i = 0;
+        int flips = 0;
+        float placeholder = 0;
+
+        while (i < nHeads){
+            Random rand = new Random();
+            float n = rand.nextFloat();
+
+            if (i == 0){
+                placeholder = n;
+            }
+
+            if (n < 0.5){
+                System.out.println("flip "+ flips + ": tails");
+                flips++;
+                placeholder = n;
+                i = 0;
+            } else if (n >= 0.5 && placeholder >= 0.5){
+                System.out.println("flip "+ flips + ": heads");
+                placeholder = n;
+                flips++;
+                i++;
+            } else {
+                flips++;
+            }
+        }
+        System.out.println("It took " + flips + " flips to get " + nHeads + " in a row.");
     }
 }
