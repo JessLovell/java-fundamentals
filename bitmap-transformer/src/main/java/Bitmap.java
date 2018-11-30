8738ae94fbe1a315f3d93ebab779e05eb9de7f05
Jessica Lovell <jessicaamberlovell@gmail.com>
2018-11-30 15:53:53 -0800

    updates to bitmap lab

--- bitmap-transformer/src/main/java/Bitmap.java
+++ bitmap-transformer/src/main/java/Bitmap.java
@@ -9,37 +9,26 @@ public class Bitmap {
     private String input;
     private String output;
     private String transform;
+    private BufferedImage imageData;
     BufferedImage img = null;
 
-    public Bitmap(String input, String output, String transform) {
+    public Bitmap (String input, String output, String transform) {
         this.input = input;
         this.output = output;
         this.transform = transform;
 
         try {
             img = ImageIO.read(new File(input));
-            System.out.println(img);
-
-            int black = 0;
-            int other = 0;
-            for (int x = 0; x < img.getHeight(); x++){
-                for (int y = 0; y < img.getWidth(); y++){
-                    int color = img.getRGB(y, x);
-                    if (color == Color.BLACK.getRGB()){
-                        black++;
-                    }
-                    else {
-
-                        other++;
-                    }
-                }
-            }
-            System.out.println(black);
-            System.out.println(other);
+            this.imageData = img;
         } catch (IOException e) {
-
         }
+    }
 
+    public boolean save (String savePath) {
+        try {
+            ImageIO.write(NEEDS_TO_GO_IN_HERE);
+        } catch (IOException e){
+        }
     }
 
 }
